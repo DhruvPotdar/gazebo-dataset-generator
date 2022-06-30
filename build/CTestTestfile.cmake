43c949d176c24e97b3239c46a2c2f5df6befa218
Dhruv Potdar <dhruvpotdar29@gmail.com>
2022-07-01 01:51:56 +0530

    added 2 scenarios

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dhruv/catkin_ws/src/gazebo-drone-sim
-# Build directory: /home/dhruv/catkin_ws/src/gazebo-drone-sim/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/dhruv/catkin_ws/src/gazebo-drone-sim/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
